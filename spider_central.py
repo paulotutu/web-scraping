c0dfd0cb08f22f07e08f7c07f044c942388cf7fb
paulotutu87 <53943949+paulotutu@users.noreply.github.com>
2020-07-18 12:03:16 -0300

    Update spider_central.py
    English coded

--- spider_central.py
+++ spider_central.py
@@ -1,10 +1,10 @@
-# Importando bibliotecas
+# Import libs
 from urllib.request import Request, urlopen, urlretrieve
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Testando Acesso a URL
+# Test URL access
 url = 'https://www.microsoft.com/pt-br/store/deals/games/xbox'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
@@ -18,40 +18,43 @@ except HTTPError as e:
 except URLError as e:
     print(e.reason)
 
-# Declarando variável cards
+# Create variables
 games = []
 game = {}
 
-# Obtendo o HTML
+# Get HTML
 response = urlopen(req)
 html = response.read().decode('utf-8')
 soup = BeautifulSoup(html, 'html.parser')
+
+# Identify page total
 pages_results = int(soup.find('p', class_= 'c-paragraph-3').getText().split()[-2])
 pages = round((pages_results / 90))
 
-# Iterando por Todas as Páginas
+# Iterate through all pages
 for i in range(0, pages*90, 90):
-  # Obtendo o HTML
+  # Get HTML and apply page number
   response = urlopen('https://www.microsoft.com/pt-br/store/deals/games/xbox?s=store&skipitems=' + str(i))
   html = response.read().decode('utf-8')
   soup = BeautifulSoup(html, 'html.parser')
-  # Obtendo as TAGs de interesse
+  # Get desired objects
   products = soup.find('div', {'id': 'productPlacementList'}).findAll('div', {'class': 'm-channel-placement-item'})
 
-  # Coletando as informações dos Cards
+  # Get desired information
   for product in products:
     game = {}
-    # Obtendo a Título
-    game['Título'] = product.find('h3', class_='c-subheading-6').getText()
-    # Obtendo o Preço
-    game['Valor'] = product.find('span', itemprop='price').getText().replace('R$', 'R$ ')
-    # Obtendo o Link
+    # Get the title
+    game['Title'] = product.find('h3', class_='c-subheading-6').getText()
+    # Get the price
+    game['Value'] = product.find('span', itemprop='price').getText().replace('R$', 'R$ ')
+    # Get the website link
     game['Link'] = 'www.microsoft.com' + product.find('a').get('href')
-    # Adicionando resultado a lista cards
+    # Add results to a list
     games.append(game)
 
-# Criando um DataFrame com os resultados
+# Create a DataFrame
 dataset = pd.DataFrame(games)
-dataset.to_excel(r'C:\\Users\\paulo\\Downloads\\games.xlsx', index = False, encoding = 'utf-8')
+# Export to Excel
+dataset.to_excel(r'C:\\Users\\games.xlsx', index = False, encoding = 'utf-8')
 
 dataset
